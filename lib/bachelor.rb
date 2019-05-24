ec9f544a4c0d4c014e50c79fcc4c6377ea14fd7d
Colin <cstatro@gmail.com>
2019-05-24 00:10:32 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ''
+  data.each do |seasons,contestants|
+    if seasons == season
+      contestants.each do |key, value|
+        winner = key["name"] unless key["status"] != "Winner"
+      end
+    end
+  end
+  winner = winner.split
+  winner[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  value_returned = ''
+  data.each do |seasons,contestants|
+      contestants.each do |key, value|
+        value_returned = key["name"] unless key["occupation"] != occupation
+      end
+  end
+  value_returned
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  value_returned = 0
+  data.each do |seasons,contestants|
+      contestants.each do |key, value|
+        value_returned += 1 unless key["hometown"] != hometown
+      end
+  end
+  value_returned
 end
 
 def get_occupation(data, hometown)
-  # code here
+  value_returned = []
+  data.each do |seasons,contestants|
+      contestants.each do |key, value|
+        value_returned << key["occupation"] unless key["hometown"] != hometown
+      end
+  end
+  value_returned[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |seasons,contestants|
+    if seasons == season
+      ages = contestants.collect do |key,value|
+        key["age"].to_f
+      end 
+    end
+  end
+  sum = 0
+  ages.each {|x| sum += x}
+  (sum / ages.length).round
 end
